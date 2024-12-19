0de1471a406742df41ccfaf4d9d5cbb044279df5
Nya <rhs3bd@gmail.com>
2024-12-19 11:46:00 +0900

    dd

---
+++ 02-backend/spring-boot-ecommerce/src/test/java/com/dsapkl/springbootecommerce/SpringBootEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsapkl.springbootecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEcommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
